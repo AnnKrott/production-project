92e677ea87b304ad84a21c6cf1e16bb865e0ee5c
AnnKrott <krotova_anastasiya@inbox.ru>
2023-12-05 22:27:57 +0300

    fix import

--- src/entities/Profile/model/types/profile.ts
+++ src/entities/Profile/model/types/profile.ts
@@ -1,4 +1,4 @@
-import { type Country, type Currency } from 'shared/constants/commom'
+import { type Country, type Currency } from 'shared/constants/common'
 
 export interface Profile {
   first: string
